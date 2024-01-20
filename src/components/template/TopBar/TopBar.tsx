763bfc969c2ea8e648600c94c49b370e16625f5e
mohammadreza <spd.mohammadreza@gmail.com>
2024-01-19 23:02:38 -0800

    topbar

--- src/components/template/TopBar/TopBar.tsx
+++ src/components/template/TopBar/TopBar.tsx
@@ -1,4 +1,11 @@
+import { useState } from "react";
+
 export default function TopBar() {
+  const [lang, setLang] = useState({
+    flag: "/img/topbar/UK.png",
+    name: "English",
+  });
+  const [isShowLangModal, setIsShowLangModal] = useState(false);
   return (
     <div className="h-[68px] px-7 py-3 bg-white sticky top-0 flex justify-between items-center">
       <div className="left flex items-center gap-10">
@@ -25,9 +32,67 @@ export default function TopBar() {
           </span>
           <i className="bi bi-bell text-xl"></i>
         </div>
+        <div
+          onClick={() => setIsShowLangModal((last) => !last)}
+          className="relative flex items-center gap-2 cursor-pointer text-a_general-80 font-semibold w-32"
+        >
+          <img className="w-10 h-[27px]" src={lang.flag} alt="" />
+          <span>{lang.name}</span>
+          <i className="bi bi-chevron-down"></i>
+          <div
+            className={`absolute ${isShowLangModal ? 'flex' : 'hidden'} flex-col gap-4 px-5 py-4 text-sm w-64 top-12 left-1/2 -translate-x-1/2 bg-white rounded-xl`}
+          >
+            <p>Select Language </p>
+            <span className="block w-full h-px bg-a_general-50"></span>
+            <div className="flex flex-col gap-6">
+              <div
+                onClick={() => {
+                  setLang({
+                    flag: "/img/topbar/UK.png",
+                    name: "English",
+                  });
+                }}
+                className="flex items-center justify-between"
+              >
+                <div className="flex gap-3 items-center">
+                  <img src={`/img/topbar/UK.png`} alt="" />
+                  <span>English</span>
+                </div>
+                {lang.name === "English" && (
+                  <div>
+                    <i className="bi bi-check2"></i>
+                  </div>
+                )}
+              </div>
+              <div
+                onClick={() => {
+                  setLang({
+                    flag: "/img/topbar/iran.png",
+                    name: "Iran",
+                  });
+                }}
+                className="flex items-center justify-between"
+              >
+                <div className="flex gap-3 items-center">
+                  <img
+                    className="w-10 h-[27px]"
+                    src={`/img/topbar/iran.png`}
+                    alt=""
+                  />
+                  <span>Iran</span>
+                </div>
+                {lang.name === "Iran" && (
+                  <div>
+                    <i className="bi bi-check2"></i>
+                  </div>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
         <div className="flex items-center gap-2">
-            <img src="/img/topbar/avatar.png" alt="" />
-            <span className="text-a_general-80">mohammadreza</span>
+          <img src="/img/topbar/avatar.png" alt="" />
+          <span className="text-a_general-80">mohammadreza</span>
         </div>
       </div>
     </div>
