92ce2a10226988dae92d6cf5eeae24c40095bab9
mohammadreza <spd.mohammadreza@gmail.com>
2024-01-23 10:42:06 -0800

    addproduct

--- src/pages/Admin/Products/Addproduct/Addproduct.tsx
+++ src/pages/Admin/Products/Addproduct/Addproduct.tsx
@@ -47,7 +47,7 @@ export default function Addproduct() {
           </div>
           <div className="py-10 border-b">
             <h2 className="font-bold">Images</h2>
-            <div className="mt-5 flex flex-col gap-2">
+            <div className="mt-5">
               {/* input file */}
               <div className="file flex items-center justify-center w-full">
                 <label
@@ -84,7 +84,115 @@ export default function Addproduct() {
               {/* input file */}
             </div>
           </div>
+          {/* price */}
+          <div className="py-5 border-b">
+            <h2 className="font-bold">Price</h2>
+            <div className="grid grid-cols-1 lg:grid-cols-2 lg:gap-5">
+              <div className="mt-5 flex flex-col gap-2">
+                <p className="text-a_general-80 text-sm">Product Price</p>
+                <input
+                  className="border w-full outline-none px-4 py-2 rounded-md"
+                  placeholder="Enter price"
+                  type="text"
+                  name=""
+                  id=""
+                />
+              </div>
+              <div className="mt-5 flex flex-col gap-2">
+                <p className="text-a_general-80 text-sm">Discount Price</p>
+                <input
+                  className="border w-full outline-none px-4 py-2 rounded-md"
+                  placeholder="Price at Discount"
+                  type="text"
+                  name=""
+                  id=""
+                />
+              </div>
+            </div>
+            <div className="mt-5 flex items-center gap-3 text-a_general-80">
+              {/* radio */}
+              <div className="">
+                <input
+                  className="hidden"
+                  type="radio"
+                  name=""
+                  id="radioprice"
+                />
+                <label
+                  className={`flex justify-start px-1 items-center relative w-11  h-6 rounded-full bg-a_general-50`}
+                  htmlFor="radioprice"
+                >
+                  <span className="absolute size-4 rounded-full bg-white "></span>
+                </label>
+              </div>
+              {/* radio */}
+              <span className="">Add tax for this product</span>
+            </div>
+            {/* price */}
+          </div>
+
+          {/* Different Options */}
+          <div className="mt-10 border-b py-5">
+            <h2 className="font-bold">Different Options</h2>
+            <div className="mt-5 flex items-center gap-3 text-a_general-80">
+              {/* radio */}
+              <div className="">
+                <input
+                  className="hidden"
+                  type="radio"
+                  name=""
+                  id="radioprice"
+                />
+                <label
+                  className={`flex justify-start px-1 items-center relative w-11  h-6 rounded-full bg-a_general-50`}
+                  htmlFor="radioprice"
+                >
+                  <span className="absolute size-4 rounded-full bg-white "></span>
+                </label>
+              </div>
+              {/* radio */}
+              <span className="">Add tax for this product</span>
+            </div>
+            <div className="grid grid-cols-1 lg:grid-cols-2 lg:gap-5">
+              <div className="mt-5 flex flex-col gap-2">
+                <p className="text-a_general-80 text-sm">Size</p>
+                <div>
+                  <select className="w-full h-10 border px-2" name="" id="">
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                  </select>
+                </div>
+              </div>
+              <div className="mt-5 flex flex-col gap-2">
+                <p className="text-a_general-80 text-sm">Value</p>
+                <div className="w-full h-10 border px-2 py-1 flex items-center justify-start gap-2">
+                  <div className="bg-a_general-50 px-2 gap-3 text-a_general-80 flex justify-between rounded-md ">
+                    <span>S</span>
+                    <i className="bi bi-x"></i>
+                  </div>
+                  <div className="bg-a_general-50 px-2 gap-3 text-a_general-80 flex justify-between rounded-md ">
+                    <span>M</span>
+                    <i className="bi bi-x"></i>
+                  </div>
+                  <div className="bg-a_general-50 px-2 gap-3 text-a_general-80 flex justify-between rounded-md ">
+                    <span>L</span>
+                    <i className="bi bi-x"></i>
+                  </div>
+                  <div className="bg-a_general-50 px-2 gap-3 text-a_general-80 flex justify-between rounded-md ">
+                    <span>XL</span>
+                    <i className="bi bi-x"></i>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* Different Options */}
+          <button className="text-a_primary-100 mt-5">Add More</button>
         </div>
+        
+
         <div className="r col-span-4 p-10 bg-white mt-10 rounded-md"></div>
       </div>
     </div>
