e5ae86db54254141c97bbbd470dd3be97dcf75c4
mohammadreza <spd.mohammadreza@gmail.com>
2024-01-28 02:46:16 -0800

    table

--- src/pages/Admin/Coupons/Coupons.tsx
+++ src/pages/Admin/Coupons/Coupons.tsx
@@ -1,11 +1,58 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+import EmpityOrder from "../../../components/template/EmpityOrder/EmpityOrder";
 
 export default function Coupons() {
+  const recentTransactions: any = [
+    {
+      id: 1,
+      name: "Jessica S.",
+      Location: "96 in stock",
+      Orders: "red",
+      Spent: "$256",
+    },
+    {
+      id: 2,
+      name: "Andrew S.",
+      Location: "23.05.2020",
+      Orders: "red",
+      Spent: "$256",
+    },
+    {
+      id: 3,
+      name: "Kevin S.",
+      Location: "23.05.2020",
+      Orders: "red",
+      Spent: "$256",
+    },
+    {
+      id: 4,
+      name: "Jack S.",
+      Location: "22.05.2020",
+      Orders: "red",
+      Spent: "$256",
+    },
+    {
+      id: 5,
+      name: "Arthur S.",
+      Location: "22.05.2020",
+      Orders: "red",
+      Spent: "$256",
+    },
+  ];
   return (
-    <div>
-      <div className="text-9xl flex flex-1 justify-center text-center h-screen items-center bg-red-600">
-        orders
+    <>
+    <div className="p-7">
+      <div className="header flex justify-between">
+        <h2 className="font-bold text-2xl">Coupons</h2>
+        <button className="bg-a_primary-100 text-white px-6 py-2 rounded-md flex items-center gap-2">
+        <i className="bi bi-plus-lg"></i>
+          <span>Create Coupon</span>
+        </button>
       </div>
+      <div className="toptable"></div>
+      <div className="table"></div>
+      <div className="downtable"></div>
     </div>
+    </>
   )
 }
