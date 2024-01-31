507b7b19a0238fd33d52844c50cb5e311a38ed87
mohammadreza <spd.mohammadreza@gmail.com>
2024-01-31 08:02:06 -0800

    translate add product

--- src/pages/Admin/Products/Addproduct/Addproduct.tsx
+++ src/pages/Admin/Products/Addproduct/Addproduct.tsx
@@ -1,52 +1,51 @@
 import { Link } from "react-router-dom";
+import Headerofpages from "../../../../components/module/Headerofpages/Headerofpages";
+import { useTranslation } from "react-i18next";
+import Button from "../../../../components/module/Button/Button";
 
 export default function Addproduct() {
+  const {t} = useTranslation()
   return (
     <div className="w-full h-44 py-8 px-10">
+      
       <div className="header">
-        <Link className="text-a_general-80" to={"/admin/products"}>
-          <i className="bi bi-arrow-left"></i>
-          <span>Back</span>
-        </Link>
-        <div className="header flex justify-between">
-          <h2 className="text-a_general-100 text-2xl font-bold">Add Product</h2>
-          <div className="flex gap-4">
-            <button className="px-5 py-2 flex gap-2 items-center bg-white text-a_primary-100 rounded border">
-              <p>cancel</p>
-            </button>
-            <button className="px-5 py-2 flex gap-2 items-center text-white bg-a_primary-100 rounded border">
-              <p>Save</p>
-            </button>
-          </div>
-        </div>
+        <Headerofpages to={"/admin/products"} back={true} title={t('addproduct')}>
+        <div className="flex gap-3 flex-col items-end lg:flex-row">
+                <Button type="White" size="sm">
+                  {t("cancel")}
+                </Button>
+                <Button type="Primary" size="sm">
+                  {t("save")}
+                </Button>
+              </div>
+        </Headerofpages>
       </div>
-
       <div className="grid grid-cols-1 xl:grid-cols-12 gap-5">
         <div className="l  xl:col-span-8 px-10 py-2 bg-white mt-10 rounded-md">
           <div className="py-10 border-b">
-            <h2 className="font-bold">Information</h2>
+            <h2 className="font-bold">{t('information')}</h2>
             <div className="mt-5 flex flex-col gap-2">
-              <p className="text-a_general-80 text-sm">Product Name</p>
+              <p className="text-a_general-80 text-sm">{t('productname')}</p>
               <input
                 className="border w-full outline-none px-4 py-2 rounded-md"
-                placeholder="Summer T-Shirt"
+                placeholder={t('summertshirt')}
                 type="text"
                 name=""
                 id=""
               />
             </div>
             <div className="mt-5 flex flex-col gap-2">
-              <p className="text-a_general-80 text-sm">Product Description</p>
+              <p className="text-a_general-80 text-sm">{t('productdescription')}</p>
               <textarea
                 className="border w-full outline-none px-4 py-2 rounded-md h-24"
-                placeholder="Product description"
+                placeholder={t('productdescription')}
                 name=""
                 id=""
               />
             </div>
           </div>
           <div className="py-10 border-b">
-            <h2 className="font-bold">Images</h2>
+            <h2 className="font-bold">{t('images')}</h2>
             <div className="mt-5">
               {/* input file */}
               <div className="file flex items-center justify-center w-full">
@@ -71,11 +70,10 @@ export default function Addproduct() {
                       />
                     </svg>
                     <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
-                      <span className="font-semibold">Click to upload</span> or
-                      drag and drop
+                      <span className="font-semibold">{t('addfile')}</span>
                     </p>
                     <p className="text-xs text-gray-500 dark:text-gray-400">
-                      SVG, PNG, JPG or GIF (MAX. 800x400px)
+                      {t('ordraganddropfiles')}
                     </p>
                   </div>
                   <input id="dropzone-file" type="file" className="hidden" />
@@ -86,23 +84,23 @@ export default function Addproduct() {
           </div>
           {/* price */}
           <div className="py-5 border-b">
-            <h2 className="font-bold">Price</h2>
+            <h2 className="font-bold">{t('price')}</h2>
             <div className="grid grid-cols-1 lg:grid-cols-2 lg:gap-5">
               <div className="mt-5 flex flex-col gap-2">
-                <p className="text-a_general-80 text-sm">Product Price</p>
+                <p className="text-a_general-80 text-sm">{t('productprice')}</p>
                 <input
                   className="border w-full outline-none px-4 py-2 rounded-md"
-                  placeholder="Enter price"
+                  placeholder={t('enterprice')}
                   type="text"
                   name=""
                   id=""
                 />
               </div>
               <div className="mt-5 flex flex-col gap-2">
-                <p className="text-a_general-80 text-sm">Discount Price</p>
+                <p className="text-a_general-80 text-sm">{t('discountprice')}</p>
                 <input
                   className="border w-full outline-none px-4 py-2 rounded-md"
-                  placeholder="Price at Discount"
+                  placeholder={t('priceatdiscount')}
                   type="text"
                   name=""
                   id=""
@@ -126,14 +124,14 @@ export default function Addproduct() {
                 </label>
               </div>
               {/* radio */}
-              <span className="">Add tax for this product</span>
+              <span className="">{t('addtaxforthisproduct')}</span>
             </div>
             {/* price */}
           </div>
 
           {/* Different Options */}
           <div className="mt-10 border-b py-5">
-            <h2 className="font-bold">Different Options</h2>
+            <h2 className="font-bold">{t('differentoptions')}</h2>
             <div className="mt-5 flex items-center gap-3 text-a_general-80">
               {/* radio */}
               <div className="">
@@ -151,11 +149,11 @@ export default function Addproduct() {
                 </label>
               </div>
               {/* radio */}
-              <span className="">Add tax for this product</span>
+              <span className="">{t('thisproducthasmultipleoptions')}</span>
             </div>
             <div className="grid grid-cols-1 lg:grid-cols-2 lg:gap-5">
               <div className="mt-5 flex flex-col gap-2">
-                <p className="text-a_general-80 text-sm">Size</p>
+                <p className="text-a_general-80 text-sm">{t('size')}</p>
                 <div>
                   <select className="w-full h-10 border px-2" name="" id="">
                     <option value="1">1</option>
@@ -166,7 +164,7 @@ export default function Addproduct() {
                 </div>
               </div>
               <div className="mt-5 flex flex-col gap-2">
-                <p className="text-a_general-80 text-sm">Value</p>
+                <p className="text-a_general-80 text-sm">{t('value')}</p>
                 <div className="w-full h-10 border px-2 py-1 flex items-center justify-start gap-2">
                   <div className="bg-a_general-50 px-2 gap-3 text-a_general-80 flex justify-between rounded-md ">
                     <span>S</span>
@@ -189,23 +187,23 @@ export default function Addproduct() {
             </div>
           </div>
           {/* Different Options */}
-          <button className="text-a_primary-100 mt-5 border-b py-3 w-full text-left">Add More</button>
+          <button className="text-a_primary-100 mt-5 border-b py-3 w-full text-left">{t('addmore')}</button>
 
           <div className="py-5 border-b">
-            <h2 className="font-bold">Shipping</h2>
+            <h2 className="font-bold">{t('shipping')}</h2>
             <div className="grid grid-cols-1 lg:grid-cols-2 lg:gap-5">
               <div className="mt-5 flex flex-col gap-2">
-                <p className="text-a_general-80 text-sm">Weight</p>
+                <p className="text-a_general-80 text-sm">{t('weight')}</p>
                 <input
                   className="border w-full outline-none px-4 py-2 rounded-md"
-                  placeholder="Enter Weight"
+                  placeholder={t('enterweight')}
                   type="text"
                   name=""
                   id=""
                 />
               </div>
               <div className="mt-5 flex flex-col gap-2">
-                <p className="text-a_general-80 text-sm">Country</p>
+                <p className="text-a_general-80 text-sm">{t('country')}</p>
                 <div>
                   <select className="w-full h-10 border px-2" name="" id="">
                     <option value="1">1</option>
@@ -233,7 +231,7 @@ export default function Addproduct() {
                 </label>
               </div>
               {/* radio */}
-              <span className="">This is digital item</span>
+              <span className="">{t('thisisdigitalitem')}</span>
             </div>
             {/* price */}
           </div>
@@ -242,58 +240,58 @@ export default function Addproduct() {
 
         <div className="r  xl:col-span-4 p-10 mt-10 rounded-md flex flex-col gap-5 *:bg-white *:p-7 *:rounded-md">
           <div className="section1">
-            <h3 className="font-bold">Categories</h3>
+            <h3 className="font-bold">{t('categories')}</h3>
             <div className="flex gap-2 mt-3 items-center rounded ">
               <input className="" type="checkbox" name="" id="checkboxCategories1" />
-              <label htmlFor="checkboxCategories1">Women</label>
+              <label htmlFor="checkboxCategories1">{t('women')}</label>
             </div>
             <div className="flex gap-2 mt-3 items-center rounded ">
               <input className="" type="checkbox" name="" id="checkboxCategories1" />
-              <label htmlFor="checkboxCategories1">Men</label>
+              <label htmlFor="checkboxCategories1">{t('men')}</label>
             </div>
             <div className="flex gap-2 mt-3 items-center rounded ">
               <input className="" type="checkbox" name="" id="checkboxCategories1" />
-              <label htmlFor="checkboxCategories1">T-Shirt</label>
+              <label htmlFor="checkboxCategories1">{t('tshirt')}</label>
             </div>
             <div className="flex gap-2 mt-3 items-center rounded ">
               <input className="" type="checkbox" name="" id="checkboxCategories1" />
-              <label htmlFor="checkboxCategories1">Hoodie</label>
+              <label htmlFor="checkboxCategories1">{t('hoodie')}</label>
             </div>
             <div className="flex gap-2 mt-3 items-center rounded ">
               <input className="" type="checkbox" name="" id="checkboxCategories1" />
-              <label htmlFor="checkboxCategories1">Dress</label>
+              <label htmlFor="checkboxCategories1">{t('hoodie')}</label>
             </div>
-            <p className="mt-3 text-a_primary-100">Create New</p>
+            <p className="mt-3 text-a_primary-100">{t('createnew')}</p>
           </div>
           <div className="section2 flex flex-col gap-4">
-            <h3 className="font-bold">Tags</h3>
+            <h3 className="font-bold">{t('tags')}</h3>
             <div className="flex flex-col gap-2">
-              <label className="text-a_general-60" htmlFor="inputTags">Add Tags</label>
-              <input className="border px-3 py-2 rounded-md outline-none" placeholder="Enter tag name" type="text" name="" id="inputTags" />
+              <label className="text-a_general-60" htmlFor="inputTags">{t('addtags')}</label>
+              <input className="border px-3 py-2 rounded-md outline-none" placeholder={t('entertagname')} type="text" name="" id="inputTags" />
             </div>
             <div className="flex justify-start gap-3 flex-wrap">
               <div className="px-2 py-1 bg-a_general-50 rounded-md text-a_general-80 flex items-center">
-                <p>T-Shirt</p>
+                <p>{t('tshirt')}</p>
                 <i className="bi bi-x"></i>
               </div>
               <div className="px-2 py-1 bg-a_general-50 rounded-md text-a_general-80 flex items-center">
-                <p>T-Shirt</p>
+                <p>{t('tshirt')}</p>
                 <i className="bi bi-x"></i>
               </div>
               <div className="px-2 py-1 bg-a_general-50 rounded-md text-a_general-80 flex items-center">
-                <p>T-Shirt</p>
+                <p>{t('tshirt')}</p>
                 <i className="bi bi-x"></i>
               </div>
             </div>
           </div>
           <div className="section3 flex flex-col gap-4">
-            <h3 className="font-bold">SEO Settings</h3>
+            <h3 className="font-bold">{t('seosettings')}</h3>
             <div className="flex flex-col gap-2">
-              <label className="text-a_general-60" htmlFor="inputTags">Title</label>
+              <label className="text-a_general-60" htmlFor="inputTags">{t('title')}</label>
               <input className="border px-3 py-2 rounded-md outline-none"  type="text" name="" id="inputTags" />
             </div>
             <div className="flex flex-col gap-2">
-              <label className="text-a_general-60" htmlFor="inputTags">Description</label>
+              <label className="text-a_general-60" htmlFor="inputTags">{t('description')}</label>
               <textarea className="border px-3 py-2 rounded-md outline-none" name="" id="inputTags" />
             </div>
             
