e1093a41995bed6487e9a13d04510adacd663dc8
mohammadreza <spd.mohammadreza@gmail.com>
2024-02-06 12:31:47 -0800

    m

--- src/pages/Admin/Inbox/Inbox.tsx
+++ src/pages/Admin/Inbox/Inbox.tsx
@@ -1,12 +1,15 @@
+import { useState } from "react";
+import CheckBox from "../../../components/module/CheckBox/CheckBox";
+
 export default function Inbox() {
+  const [isChecked1,setIsChecked1] = useState(false)
+  const [isChecked2,setIsChecked2] = useState(false)
   return (
     <>
-      <div className="size-52 bg-white dark:bg-a_general-90 dark:bg-gray-900 shadow-lg mt-20">
-
-      </div>
-      <div>
-        <button >click</button>
-      </div>
+    <div className="flex flex-col gap-5">
+    <CheckBox setIsChecked={setIsChecked1} forid="mohammad"></CheckBox>
+    <CheckBox setIsChecked={setIsChecked2} forid="ali"></CheckBox>
+    </div>
     </>
   );
 }
