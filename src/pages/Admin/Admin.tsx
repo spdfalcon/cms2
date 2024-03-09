618da68bf694eba3403a7b79375f9949d584183b
mohammadreza <spd.mohammadreza@gmail.com>
2024-03-08 21:54:46 -0800

    login

--- src/pages/Admin/Admin.tsx
+++ src/pages/Admin/Admin.tsx
@@ -5,7 +5,7 @@ import React, { useState } from "react";
 
 const Admin: React.FC = () => {
   const [isShowHamberMenu, setIsShowHamberMenu] = useState(false);
-  
+
   return (
     <>
       <div className="relative">
@@ -15,31 +15,38 @@ const Admin: React.FC = () => {
         ></TopBar>
         <div className="flex ">
           <div className="hidden md:flex w-56">
-          <div className="fixed">
-            <SideBar
-              isShowHamberMenu={isShowHamberMenu}
-              setIsShowHamberMenu={setIsShowHamberMenu}
-            ></SideBar>
-          </div>
+            <div className="fixed">
+              <SideBar
+                isShowHamberMenu={isShowHamberMenu}
+                setIsShowHamberMenu={setIsShowHamberMenu}
+              ></SideBar>
+            </div>
           </div>
           <div className="flex-1 container px-5 mt-12">
             <Outlet></Outlet>
           </div>
         </div>
-        
+
         <div
           className={`z-20 mt-[68px] duration-300 absolute top-0 md:hidden  ${
-            isShowHamberMenu ? "w-48 ltr:left-0 rtl:right-0" : "w-0 ltr:-left-52 rtl:-right-52"
+            isShowHamberMenu
+              ? "w-48 ltr:left-0 rtl:right-0"
+              : "w-0 ltr:-left-52 rtl:-right-52"
           }`}
         >
           <div className="fixed ">
-          <SideBar
-            isShowHamberMenu={isShowHamberMenu}
-            setIsShowHamberMenu={setIsShowHamberMenu}
-          ></SideBar>
+            <SideBar
+              isShowHamberMenu={isShowHamberMenu}
+              setIsShowHamberMenu={setIsShowHamberMenu}
+            ></SideBar>
           </div>
         </div>
-        <div onClick={()=>setIsShowHamberMenu((last:any)=>!last)} className={`md:hidden z-10  duration-300 absolute top-0 h-full bg-a_general-30/50 dark:bg-a_general-30/20 ${isShowHamberMenu ? 'w-full' : 'w-0'}`}></div>
+        <div
+          onClick={() => setIsShowHamberMenu((last: any) => !last)}
+          className={`md:hidden z-10  duration-300 absolute top-0 h-full bg-a_general-30/50 dark:bg-a_general-30/20 ${
+            isShowHamberMenu ? "w-full" : "w-0"
+          }`}
+        ></div>
       </div>
     </>
   );
