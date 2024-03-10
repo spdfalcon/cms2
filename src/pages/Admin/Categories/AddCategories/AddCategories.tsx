6f166bbf8c9463a247dd1a07e12d84c3007a3b44
mohammadreza <spd.mohammadreza@gmail.com>
2024-03-10 00:42:09 -0800

    mohammad

--- src/pages/Admin/Categories/AddCategories/AddCategories.tsx
+++ src/pages/Admin/Categories/AddCategories/AddCategories.tsx
@@ -4,26 +4,39 @@ import Button from "../../../../components/module/Button/Button";
 import { useTranslation } from "react-i18next";
 import CheckBox from "../../../../components/module/CheckBox/CheckBox";
 import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 export default function AddCategories() {
-  const products = [
-    {id:1, pic: "/img/dashboard/11.png", title: "Women Striped T-Shirt" },
-    {id:2, pic: "/img/dashboard/11.png", title: "Women Striped T-Shirt" },
-    {id:3, pic: "/img/dashboard/11.png", title: "Women Striped T-Shirt" },
-    {id:4, pic: "/img/dashboard/11.png", title: "Women Striped T-Shirt" },
-    {id:5, pic: "/img/dashboard/11.png", title: "Women Striped T-Shirt" },
-    {id:6, pic: "/img/dashboard/11.png", title: "Women Striped T-Shirt" },
-    {id:7, pic: "/img/dashboard/11.png", title: "Women Striped T-Shirt" },
-    {id:8, pic: "/img/dashboard/11.png", title: "Women Striped T-Shirt" },
-    {id:9, pic: "/img/dashboard/11.png", title: "Women Striped T-Shirt" },
-    {id:10, pic: "/img/dashboard/11.png", title: "Women Striped T-Shirt" },
-    {id:11, pic: "/img/dashboard/11.png", title: "Women Striped T-Shirt" },
-    {id:12, pic: "/img/dashboard/11.png", title: "Women Striped T-Shirt" },
-  ];
-  const {t} = useTranslation()
-  const [ischeckedaddcategoriesvisibleonsite , setIscheckedaddcategoriesvisibleonsite] = useState(false)
+  const { register, handleSubmit, reset } = useForm({
+    defaultValues:{
+      name:''
+    }
+  });
+  const [products , setProducts] = useState([
+    { id: crypto.randomUUID(), pic: "/img/dashboard/11.png", title: "Women Striped T-Shirt" },
+    { id: crypto.randomUUID(), pic: "/img/dashboard/11.png", title: "Women Striped " },
+    { id: crypto.randomUUID(), pic: "/img/dashboard/11.png", title: "Women  T-Shirt" },
+  ])
+  const formSubmit = (data: any) => {
+    const newcategory = {
+      name:data.name,
+      products,
+      image:null,
+      visibale:ischeckedaddcategoriesvisibleonsite
+    }
+    
+    
+    
+    
+    
+  };
+  const { t } = useTranslation();
+  const [
+    ischeckedaddcategoriesvisibleonsite,
+    setIscheckedaddcategoriesvisibleonsite,
+  ] = useState(false);
   return (
-    <div>
+    <form onSubmit={handleSubmit(formSubmit)}>
       <div className=" py-9 ">
         <div className="header">
           <Headerofpages
@@ -44,12 +57,17 @@ export default function AddCategories() {
         <div className="mt-8 *:rounded-md *:p-7 grid grid-cols-1 xl:grid-cols-12 gap-4">
           <div className="l xl:col-span-8 bg-white dark:bg-a_general-90">
             <div className="flex gap-2">
-              <p className="font-bold dark:text-white rtl:font-iransans-700">{t('products')}</p>
+              <p className="font-bold dark:text-white rtl:font-iransans-700">
+                {t("products")}
+              </p>
               <span className="text-a_general-60">{products.length}</span>
             </div>
             <div className="mt-5 flex flex-col gap-3">
               {products.map((item) => (
-                <div key={item.id} className="rounded-md px-5 py-4 border flex items-center gap-4 justify-between">
+                <div
+                  key={item.id}
+                  className="rounded-md px-5 py-4 border flex items-center gap-4 justify-between"
+                >
                   <div className="left flex items-center gap-2 text-xs md:text-base">
                     <i className="bi bi-three-dots-vertical text-a_general-60"></i>
                     <img src="/img/dashboard/11.png" alt="" />
@@ -66,32 +84,51 @@ export default function AddCategories() {
                 to={""}
               >
                 <i className="bi bi-plus-lg"></i>
-                <span>{t('addproduct')}</span>
+                <span>{t("addproduct")}</span>
               </Link>
             </div>
           </div>
+
           <div className="r *:bg-white dark:*:bg-a_general-70 dark:bg-a_general-90 *:p-5 *:rounded-md xl:col-span-4 flex flex-col gap-5">
             <div className="flex flex-col gap-5">
-              <h3 className="font-bold dark:text-white rtl:font-iransans-700">{t('categoryvisibility')}</h3>
+              <h3 className="font-bold dark:text-white rtl:font-iransans-700">
+                {t("categoryvisibility")}
+              </h3>
               <div>
                 <div className="flex gap-3">
-                  <CheckBox setIsChecked={setIscheckedaddcategoriesvisibleonsite} ischecked={ischeckedaddcategoriesvisibleonsite} forid="addcategoriesvisibleonsite"></CheckBox>
-                  <label htmlFor="addcategoriesvisibleonsite" className="text-xs md:text-base">{t('visibleonsite')}</label>
+                  <CheckBox
+                    setIsChecked={setIscheckedaddcategoriesvisibleonsite}
+                    ischecked={ischeckedaddcategoriesvisibleonsite}
+                    forid="addcategoriesvisibleonsite"
+                  ></CheckBox>
+                  <label
+                    htmlFor="addcategoriesvisibleonsite"
+                    className="text-xs md:text-base"
+                  >
+                    {t("visibleonsite")}
+                  </label>
                 </div>
               </div>
             </div>
             <div className="flex flex-col gap-4">
               <div>
-                <h3 className="font-bold dark:text-white rtl:font-iransans-700">{t('categoryinfo')}</h3>
+                <h3 className="font-bold dark:text-white rtl:font-iransans-700">
+                  {t("categoryinfo")}
+                </h3>
               </div>
               <div className="flex flex-col gap-2">
-                <label className="text-a_general-60 text-xs md:text-base" htmlFor="">
-                  {t('name')}
+                <label
+                  className="text-a_general-60 text-xs md:text-base"
+                  htmlFor=""
+                >
+                  {t("name")}
                 </label>
                 <input
+                  {...register("name", {
+                    required: t("pleaseentertheproductname"),
+                  })}
                   className="border rounded-md px-2 py-1"
                   type="text"
-                  name=""
                   id=""
                 />
               </div>
@@ -100,14 +137,16 @@ export default function AddCategories() {
                   className="flex flex-col gap-2 cursor-pointer"
                   htmlFor="productfile"
                 >
-                  <p className="text-a_general-60 text-xs md:text-base">{t('images')}</p>
+                  <p className="text-a_general-60 text-xs md:text-base">
+                    {t("images")}
+                  </p>
                   <div className="border-2 border-dashed h-44 flex justify-center items-center">
                     <div className="flex flex-col items-center gap-3">
                       <div className="border px-4 py-1 rounded-md text-a_primary-100 text-sm md:text-base">
-                        {t('addfile')}
+                        {t("addfile")}
                       </div>
                       <p className="text-a_general-60 text-xs md:text-base">
-                        {t('ordraganddropfiles')}
+                        {t("ordraganddropfiles")}
                       </p>
                     </div>
                   </div>
@@ -123,6 +162,6 @@ export default function AddCategories() {
           </div>
         </div>
       </div>
-    </div>
+    </form>
   );
 }
--- src/pages/Admin/Categories/Categories.tsx
+++ src/pages/Admin/Categories/Categories.tsx
@@ -2,61 +2,79 @@ import { Link } from "react-router-dom";
 import Headerofpages from "../../../components/module/Headerofpages/Headerofpages";
 import Button from "../../../components/module/Button/Button";
 import { useTranslation } from "react-i18next";
+import { useQuery } from "react-query";
+import apiRequests from "../../../configs/axios/apiRequests";
+import Cookies from "universal-cookie";
+import Loading from "../../../components/module/Loading/Loading";
 
 export default function Categories() {
-  const categories = [
-    { id: 1, pic: "/img/dashboard/11.png", title: "Men Clothes", count: "24" },
-    { id: 2, pic: "/img/dashboard/11.png", title: "Men Clothes", count: "24" },
-    { id: 3, pic: "/img/dashboard/11.png", title: "Men Clothes", count: "24" },
-    { id: 4, pic: "/img/dashboard/11.png", title: "Men Clothes", count: "24" },
-    { id: 5, pic: "/img/dashboard/11.png", title: "Men Clothes", count: "24" },
-    { id: 6, pic: "/img/dashboard/11.png", title: "Men Clothes", count: "24" },
-    { id: 7, pic: "/img/dashboard/11.png", title: "Men Clothes", count: "24" },
-    { id: 8, pic: "/img/dashboard/11.png", title: "Men Clothes", count: "24" },
-    { id: 9, pic: "/img/dashboard/11.png", title: "Men Clothes", count: "24" },
-  ];
-  const {t} = useTranslation()
+  const cookies = new Cookies();
+  const token = cookies.get("token");
+  const { data: categories, isLoading } = useQuery("categories", () =>
+    apiRequests
+      .get("/category", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((res) => {
+        return res.data;
+      })
+  );
+  const { t } = useTranslation();
   return (
+    <>
     <div className="py-8">
       {/* header */}
-      
       <div>
-      <Headerofpages title={t('categories')}>
-        <Link to={'/admin/categories/addcategory'}>
-          <Button type="Primary" icon="bi bi-plus-lg" size="md">
-            {t('addcategory')}
-          </Button>
-        </Link>
+        <Headerofpages title={t("categories")}>
+          <div onClick={()=>console.log('mohammad')
+          }>
+            <Button type="Primary" icon="bi bi-plus-lg" size="md">
+              {t("addcategory")}
+            </Button>
+          </div>
         </Headerofpages>
       </div>
       {/* header */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 mt-10 gap-5">
-        {categories.map((item) => (
-          <div
-            key={item.id}
-            className=" flex flex-col gap-3 pb-3 bg-white dark:bg-a_general-90 rounded-md overflow-hidden group "
-          >
-            <div className="relative flex justify-center">
-              <img
-                className=" object-contain"
-                src="/img/category/11.png"
-                alt=""
-
-              />
-              <Link className="group-hover:bg-a_general-70/40 hidden group-hover:flex w-full h-full justify-center items-center z-50 text-white absolute top-1/2 -translate-y-1/2 right-1/2 translate-x-1/2" to={`${item.id}`}>
-                <span className="px-3 bg-white dark:bg-a_general-90 rounded text-a_primary-100 flex gap-2">
-              <i className="bi bi-pencil"></i>
-                  <span>Edit</span>
+      {isLoading ? (
+        <Loading></Loading>
+      ) : (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 mt-10 gap-5">
+          {categories?.map((item: any) => (
+            <div
+              key={item.id}
+              className=" flex flex-col gap-3 pb-3 bg-white dark:bg-a_general-90 rounded-md overflow-hidden group "
+            >
+              <div className="relative flex justify-center">
+                <img
+                  className=" object-contain"
+                  src="/img/category/11.png"
+                  alt=""
+                />
+                <Link
+                  className="group-hover:bg-a_general-70/40 hidden group-hover:flex w-full h-full justify-center items-center z-50 text-white absolute top-1/2 -translate-y-1/2 right-1/2 translate-x-1/2"
+                  to={`${item.id}`}
+                >
+                  <span className="px-3 bg-white dark:bg-a_general-90 rounded text-a_primary-100 flex gap-2">
+                    <i className="bi bi-pencil"></i>
+                    <span>Edit</span>
+                  </span>
+                </Link>
+              </div>
+              <div className="px-3">
+                <p className="font-bold dark:text-white rtl:font-iransans-700">
+                  {item.name}
+                </p>
+                <span className="text-a_general-60">
+                  {item.products.length} items
                 </span>
-              </Link>
+              </div>
             </div>
-            <div className="px-3">
-              <p className="font-bold dark:text-white rtl:font-iransans-700">Men Clothes</p>
-              <span className="text-a_general-60">24 items</span>
-            </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      )}
     </div>
+    </>
   );
 }
