166624c822dbc5dee5ab5ce5540bd18766b0ecc0
mohammadreza <spd.mohammadreza@gmail.com>
2024-03-17 06:05:46 -0700

    mohammad

--- src/pages/Admin/Customers/Customers.tsx
+++ src/pages/Admin/Customers/Customers.tsx
@@ -3,8 +3,18 @@ import EmpityOrder from "../../../components/template/EmpityOrder/EmpityOrder";
 import Headerofpages from "../../../components/module/Headerofpages/Headerofpages";
 import Button from "../../../components/module/Button/Button";
 import { useTranslation } from "react-i18next";
+import Pagination from "../../../components/module/Pagination/Pagination";
+import { useEffect, useState } from "react";
+import apiRequests from "../../../configs/axios/apiRequests";
+import getCookies from "../../../configs/cookies/getcookise";
+import { useQuery } from "react-query";
+import Filter from "../../../components/module/Filter/Filter";
 
 export default function Customers() {
+  const { t } = useTranslation();
+  const paginationInPage = 2;
+  const [page, setPage] = useState(1);
+  const [searchInput, setSearchInput] = useState("");
   const recentTransactions: any = [
     {
       id: 1,
@@ -42,20 +52,82 @@ export default function Customers() {
       Spent: "$256",
     },
   ];
-  const { t } = useTranslation();
+  const token = getCookies();
+  const { data: customers } = useQuery(
+    "Customers",
+    () =>
+      apiRequests
+        .get("/user", {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        })
+        .then((res) => res.data)
+    // ,{
+    //   refetchInterval:2000
+    // }
+  );
+  console.log(customers);
+  const [orders, setOrders]: any = useState([]);
+  useEffect(() => {
+    if (customers) {
+      setOrders([...customers]);
+    }
+  }, [customers]);
+  useEffect(() => {
+    if (customers) {
+      const neworder = [...customers].filter((item) =>
+        item.first_name.toLowerCase().includes(searchInput.toLowerCase())
+      );
+      if (searchInput) {
+        setPage(1);
+        setOrders(neworder);
+      } else {
+        setOrders(customers);
+      }
+    }
+  }, [searchInput]);
+  const [nameFilter, setNameFilter] = useState(t("filter"));
+  const filters = [
+    { id: 8, name: t("all") },
+    { id: 1, name: t("rating") },
+    { id: 7, name: t("price") },
+  ];
+  const filterHandler = (e: any) => {
+    switch (e.target.innerHTML) {
+      case t("price"): {
+        const newOrder = [...customers].sort(
+          (a, b) => b.order_count - a.order_count
+        );
+        setOrders(newOrder);
+        break;
+      }
+      case t("rating"): {
+        const newOrder = [...customers].sort(
+          (a, b) => b?.purchase_amount - a?.purchase_amount
+        );
+        setOrders(newOrder);
+        break;
+      }
+      case t("all"): {
+        setOrders(customers);
+        break;
+      }
+    }
+  };
   return (
     <div className=" py-[30px] flex flex-col gap-y-10">
       <div className="header">
         <Headerofpages title={t("customers")}>
           <div className="flex flex-col md:flex-row gap-2 items-center justify-center ">
-          <Button size="sm" type="White">
-            {t("export")}
-          </Button>
-          <Link to={"addcustomer"}>
-            <Button icon="bi bi-plus-lg" size="sm" type="Primary">
-              {t("addcustomer")}
+            <Button size="sm" type="White">
+              {t("export")}
             </Button>
-          </Link>
+            <Link to={"addcustomer"}>
+              <Button icon="bi bi-plus-lg" size="sm" type="Primary">
+                {t("addcustomer")}
+              </Button>
+            </Link>
           </div>
         </Headerofpages>
       </div>
@@ -63,9 +135,12 @@ export default function Customers() {
         <div className="bg-white dark:bg-a_general-90 py-8 px-7 rounded-lg grid grid-cols-1 overflow-x-auto">
           <div className="header flex justify-between">
             <div className="left flex gap-4">
-              <div className="l border w-44 h-9 rounded-md flex justify-between items-center py-2 px-4 text-a_general-60 cursor-pointer">
-                <span>{t('filter')}</span>
-                <i className="bi bi-chevron-down"></i>
+              <div onClick={filterHandler}>
+                <Filter
+                  nameFilter={nameFilter}
+                  setNameFilter={setNameFilter}
+                  filter={filters}
+                ></Filter>
               </div>
               <div className="l border max-w-[350px] h-9 rounded-md flex gap-3 items-center  text-a_general-60 relative overflow-hidden">
                 <label
@@ -73,11 +148,13 @@ export default function Customers() {
                   className="bi bi-search absolute left-4"
                 ></label>
                 <input
-                  placeholder={`${t('search')}...`}
+                  placeholder={`${t("search")}...`}
                   className="w-full h-full ps-14 text-a_general-90 outline-none"
                   type="search"
                   name=""
                   id="searchorder"
+                  value={searchInput}
+                  onChange={(e) => setSearchInput(e.target.value)}
                 />
               </div>
             </div>
@@ -95,67 +172,70 @@ export default function Customers() {
                   </tr>
                 </thead>
                 <tbody className=" mt-5">
-                  {recentTransactions.map((item: any) => (
-                    <tr key={item.id} className="*:px-6 *:py-3 *:text-nowrap">
-                      <td className="text-a_general-100 dark:text-white font-medium text-xs md:text-sm  flex items-center gap-2">
-                        <div className="size-8 flex justify-center items-center uppercase bg-a_general-60 rounded-full font-bold dark:text-white rtl:font-iransans-700 text-white">
-                          {item.name[0]}
-                        </div>
-                        <label htmlFor={`checkbox${item.id}`} className="">
-                          {item.name}
-                        </label>
-                      </td>
-                      <td className="text-a_general-100 dark:text-white text-xs md:text-sm">
-                        {item.Location}
-                      </td>
-                      <td className="text-a_general-100 dark:text-white text-xs md:text-sm">
-                        {item.Orders}
-                      </td>
-                      <td className="">
-                        <span
-                          className={`px-4 py-1 rounded-md ${
-                            item.paymentstatus === "Paid"
-                              ? "bg-a_green-101/20  text-a_green-101"
-                              : "bg-a_general-80/15 text-a_general-80 dark:text-a_general-40"
-                          }`}
-                        >
-                          {item.Spent}
-                        </span>
-                      </td>
-                      <td>
-                        <div className="right text-a_primary-100  flex gap-2 justify-center">
-                          <Link
-                            to={`${item.id}`}
-                            className="border size-9 flex cursor-pointer justify-center items-center rounded-md hover:bg-a_primary-100 hover:text-white duration-300"
-                          >
-                            <i className="bi bi-pencil"></i>
-                          </Link>
-                          <div className="border size-9 flex cursor-pointer justify-center items-center rounded-md hover:bg-a_primary-100 hover:text-white duration-300">
-                            <i className="bi bi-trash"></i>
+                  {orders
+                    .slice(
+                      paginationInPage * page - paginationInPage,
+                      paginationInPage * page
+                    )
+                    ?.map((item: any) => (
+                      <tr key={item.id} className="*:px-6 *:py-3 *:text-nowrap">
+                        <td className="text-a_general-100 dark:text-white font-medium text-xs md:text-sm  flex items-center gap-2">
+                          <div className="size-8 flex justify-center items-center uppercase bg-a_general-60 rounded-full font-bold dark:text-white rtl:font-iransans-700 text-white">
+                            {item.first_name[0]}
                           </div>
-                        </div>
-                      </td>
-                    </tr>
-                  ))}
+                          <label htmlFor={`checkbox${item.id}`} className="">
+                            {item.first_name}
+                          </label>
+                        </td>
+                        <td className="text-a_general-100 dark:text-white text-xs md:text-sm">
+                          {item.email}
+                        </td>
+                        <td className="text-a_general-100 dark:text-white text-xs md:text-sm">
+                          {item.order_count}
+                        </td>
+                        <td className="">
+                          <span
+                            className={`px-4 py-1 rounded-md ${
+                              item.paymentstatus === "Paid"
+                                ? "bg-a_green-101/20  text-a_green-101"
+                                : "bg-a_general-80/15 text-a_general-80 dark:text-a_general-40"
+                            }`}
+                          >
+                            {`${item.purchase_amount} ريال `}
+                          </span>
+                        </td>
+                        <td>
+                          <div className="right text-a_primary-100  flex gap-2 justify-center">
+                            <Link
+                              to={`${item.id}`}
+                              className="border size-9 flex cursor-pointer justify-center items-center rounded-md hover:bg-a_primary-100 hover:text-white duration-300"
+                            >
+                              <i className="bi bi-pencil"></i>
+                            </Link>
+                            <div className="border size-9 flex cursor-pointer justify-center items-center rounded-md hover:bg-a_primary-100 hover:text-white duration-300">
+                              <i className="bi bi-trash"></i>
+                            </div>
+                          </div>
+                        </td>
+                      </tr>
+                    ))}
                 </tbody>
               </table>
             </div>
           </div>
           <div className="flex down justify-between items-center gap-2 text-a_general-70">
-            <div className="l flex gap-2 items-center">
-              <i className="bi bi-arrow-left cursor-pointer"></i>
-              <ul className="flex *:flex *:justify-center *:items-center gap-2 *:size-7 *:rounded *:cursor-pointer">
-                <li>1</li>
-                <li className="bg-a_primary-30">2</li>
-                <li>3</li>
-                <li>4</li>
-                <li>5</li>
-                <li>6</li>
-              </ul>
-              <i className="bi bi-arrow-right cursor-pointer"></i>
+            <div className="flex down justify-between text-a_general-70 w-full">
+              <Pagination
+                all={orders.length}
+                inpage={paginationInPage}
+                setPage={setPage}
+                page={page}
+              ></Pagination>
             </div>
             <div className="r text-nowrap text-xs md:text-base">
-              <p>{recentTransactions.length} {t('results')}</p>
+              <p>
+                {orders.length} {t("results")}
+              </p>
             </div>
           </div>
         </div>
